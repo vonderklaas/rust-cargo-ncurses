897a857a5e38fa6a8476591786d81b4d43caed73
Nick Garbalau <vonderklaas@proton.me>
2025-07-14 14:42:26 +0700

    fix: add missing user init implementation query

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -10,7 +10,7 @@ type Storage struct {
 		Create(context.Context, *Post) error
 	}
 	Users interface {
-		Create(context.Context) error
+		Create(context.Context, *User) error
 	}
 }
 
--- internal/store/users.go
+++ internal/store/users.go
@@ -19,6 +19,26 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, email, password)
+		VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Email,
+		user.Password,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
