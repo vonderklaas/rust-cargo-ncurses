fbbd18282c3bbe5f3cae137da2e01c4973276097
Nick Garbalau <vonderklaas@proton.me>
2025-07-17 12:30:35 +0700

    fix: fix migrations

--- cmd/migrate/migrations/000002_posts_create.up.sql
+++ cmd/migrate/migrations/000002_posts_create.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS posts(
     id bigserial PRIMARY KEY,
-	title text NOT NULL,
+    title text NOT NULL,
     user_id bigint NOT NULL,
     content text NOT NULL,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- cmd/migrate/migrations/000003_alter_posts_table.down.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE posts DROP CONSTRAINT fk_user;
